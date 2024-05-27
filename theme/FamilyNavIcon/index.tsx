c1a42eef98b69cd0522670e98d92cb018f07ef23
liangxiaoming <liangxiaoming@yy.com>
2024-05-27 13:10:07 +0800

    feat: add family navicon

--- theme/FamilyNavIcon/index.tsx
+++ theme/FamilyNavIcon/index.tsx
@@ -22,6 +22,7 @@ export const FamilyNavIcon = (props: FamilyNavIconProps = {}) => {
       >
         <div className={style.wrapper}>
           <svg width="20" height="20" fill="none">
+            <title>logo</title>
             <path
               d="M2.5 5H5V2.5H2.5V5zM5 11.25H2.5v-2.5H5v2.5zm12.5 0H15v-2.5h2.5v2.5zm-8.75 0h2.5v-2.5h-2.5v2.5zM5 17.5H2.5V15H5v2.5zm10 0h2.5V15H15v2.5zm-3.75 0h-2.5V15h2.5v2.5zM17.5 5H15V2.5h2.5V5zM8.75 5h2.5V2.5h-2.5V5z"
               fill="currentColor"
